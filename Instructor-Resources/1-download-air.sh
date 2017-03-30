8acc45857a5d4083b26ffe847dabbf210d753841
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-03-30 09:20:58 -0400

    remove rstudio install from air download

--- Instructor-Resources/1-download-air.sh
+++ Instructor-Resources/1-download-air.sh
@@ -11,9 +11,3 @@ hdfs dfs -mkdir /FlightData/
 
 hdfs dfs -copyFromLocal AirlineSubsetCsv/ /FlightData/
 hdfs dfs -copyFromLocal WeatherSubsetCsv /FlightData/
-
-Rscript ./install-pkgs.R
-
-wget https://raw.githubusercontent.com/akzaidi/etc/master/inst/install-rstudio-ubuntu.sh
-chmod +x ./install-rstudio-ubuntu.sh
-./install-rstudio-ubuntu.sh
